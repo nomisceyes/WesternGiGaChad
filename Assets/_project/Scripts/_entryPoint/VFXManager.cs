2bf1f4eb9ca779a8dee5bec9a58bff69fda38f55
nomisceyes <157041379+nomisceyes@users.noreply.github.com>
2026-04-15 16:24:39 +0300

    Update

--- Assets/_project/Scripts/_entryPoint/VFXManager.cs
+++ Assets/_project/Scripts/_entryPoint/VFXManager.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class VFXManager : MonoBehaviour, IService
 {
     public ParticleSystem GhostDeathVFX;
     public ParticleSystem RifleShootVFX;
     public ParticleSystem HitVFX;
-    
+
+    private GameObject _vfx;
+
     public void Init()
     {
-        GhostDeathVFX =  Instantiate(Res.VFX.GhostDeathVFX);
-        RifleShootVFX = Instantiate(Res.VFX.RifleShootVFX);
-        HitVFX = Instantiate(Res.VFX.HitVFX);
+        _vfx = new GameObject("VFXManager");
+
+        GhostDeathVFX = InstantiateAndPersist(Res.VFX.GhostDeathVFX);
+        RifleShootVFX = InstantiateAndPersist(Res.VFX.RifleShootVFX);
+        HitVFX = InstantiateAndPersist(Res.VFX.HitVFX);
+
+        DontDestroyOnLoad(_vfx);
+    }
+
+    private ParticleSystem InstantiateAndPersist(ParticleSystem prefab)
+    {
+        var instance = Instantiate(prefab, transform);
+        _vfx.transform.parent = instance.transform;
+        DontDestroyOnLoad(instance.gameObject);
+        return instance;
     }
 }
